09fb72c075efc07ab34b0a6c4985540e0ee266a9
The Holy Goose <rikerfu+thegoose@gmail.com>
2026-07-31 23:24:29 -0700

    Yay!

--- pages/credentials/additional-screening/xcript.js
+++ pages/credentials/additional-screening/xcript.js
@@ -5,20 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
     return null;
   }
   
-  if (getCookie('site_access') === 'granted' && window.location.pathname !== '/' || getCookie('need_additional_screening') !== 'true') {
-    window.location.href = 'https://thegoosesite.github.io'; 
+  if ((getCookie('site_access') === 'granted' && window.location.pathname !== '/') || getCookie('need_additional_screening') !== 'true') {
+    window.location.href = 'https://thegoosesite.github.io/pages/welcome'; 
   }
-});
-document.querySelector(".sq-btn").addEventListener("click", ()=>{
-    const STATUS = document.querySelector(".sq-status");
-    const QUESTION = document.querySelector("#sq");
-    const VALUE = QUESTION.value;
-    if (VALUE === "great-grandnephew"){
-        setTimeout(() => {
-            STATUS.textContent = "Status: Passed. Redirecting...";
-            document.cookie = "need_additional_screening=true; max-age=" + (60 * 60 * 24 * 7) + "; path=/; SameSite=Strict";
-        },300)
-    } else {
-        STATUS.textContent = "Status: Failed. Please try again."
-    }
+  const BUTTON = document.querySelector(".sq-btn");
+  if (BUTTON) {
+        BUTTON.addEventListener("click", ()=>{
+            const STATUS = document.querySelector(".sq-status");
+            const QUESTION = document.querySelector("#sq");
+            const VALUE = QUESTION.value;
+            console.log(VALUE); // to check with it
+            if (VALUE === "great-grandnephew"){
+                setTimeout(() => {
+                    STATUS.textContent = "Status: Passed. Redirecting...";
+                    document.cookie = "site_access=granted; max-age=" + (60 * 60 * 24 * 7) + "; path=/; SameSite=Strict";
+                    window.location.replace('https://thegoosesite.github.io')
+                },300)
+            } else {
+                STATUS.textContent = "Status: Failed. Please try again."
+            }
+        });
+  }
 });
