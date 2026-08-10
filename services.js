f7dae8876117acbfb12a81ec2030231f6d532acb
🪿 Goose Host 🪿 <rikerfu+thegoosesite@gmail.com>
2026-08-09 19:54:06 -0700

    Create services.js

---
+++ services.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+  // For gosedevs: Set this manually
+  siteRunning = false;
+  if (siteRunning === true){
+    console.log(""); // DONT add anything here
+  }else{
+    window.location.replace("https://worshipthegoose.github.io/cdn4/server_troubles.html"); // Server troubles
+  }
+});
