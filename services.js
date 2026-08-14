f6096920381bd02ff3304bf3e3484bae89341cd2
🪿 Goose Host 🪿 <rikerfu+thegoosesite@gmail.com>
2026-08-14 12:51:04 -0700

    Update services.js

--- services.js
+++ services.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
   // For gosedevs: Set this manually
   siteRunning = true;
   if (siteRunning === true){
+    /*
     if (window.location.origin == "https://worshipthegoose.github.io" || window.location.origin == "https://thegoosesite.github.io"){
       console.log("You are viewing an official The Goose Site page. gOoSe.");
     }else{
@@ -10,6 +11,8 @@ document.addEventListener("DOMContentLoaded", function(){
       document.body.innerHTML = "<p>This site is trying to pirate The Goose Site. <a href='about:blank'>Return to safety</a></p>";
       
     }
+    */
+    
   }else{
     window.location.replace("https://worshipthegoose.github.io/cdn4/deactivation.html"); // Server troubles
   }
