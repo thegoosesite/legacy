3af72584f43e32b191875c35a20b72ef0635d817
🪿 Goose Host 🪿 <rikerfu+thegoosesite@gmail.com>
2026-08-16 22:59:24 -0700

    Update services.js

--- services.js
+++ services.js
@@ -1,12 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(){
-  // For gosedevs: Set this manually
+document.addEventListener("DOMContentLoaded", function () {
+  // Constants and Nonstants
   const manitarget = "https://worshipthegoose.github.io/cdn4/deactivation.html";
-  const siteRunning = false;
-  if (siteRunning === true){
-     if (window.location.href === manitarget){
-       window.location.replace("https://thegoosesite.github.io")
-     }
-  }else{
-    window.location.replace(manitarget); // Server troubles
+  let siteRunning = false; // For gosedevs: Set this manually
+
+  if (siteRunning) {
+    if (window.location.href === manitarget) {
+      window.location.replace("https://thegoosesite.github.io");
+    }
+  } else {
+    if (window.location.href !== manitarget) {
+      window.location.replace(manitarget);
+    }
   }
 });
