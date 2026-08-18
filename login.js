44a0b0f5e8ea5d25ca31555e83d371ca4954ac34
🪿 Goose Host 🪿 <rikerfu+thegoosesite@gmail.com>
2026-08-17 22:14:45 -0700

    Update login.js

--- login.js
+++ login.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const footer = document.querySelector("footer");
   
   if (footer) {
-    footer.insertAdjacentHTML("beforeend", "<center><a class='eyecare'>Eye Care Mode</a></center>");
+    footer.insertAdjacentHTML("beforeend", "<center><a class='eyecare'><u>Eye Care Mode</u></a></center>");
   }
 
   const icare = document.querySelector(".eyecare");
