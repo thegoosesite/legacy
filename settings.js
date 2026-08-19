fc1a4752396c028b99979bf780802a940312e814
🪿 Goose Host 🪿 <rikerfu+thegoosesite@gmail.com>
2026-08-19 15:30:04 -0700

    xz

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@
                     <li class="gooset-themes-li">Themes</li>
                     <li class="gooset-about-li">About</li>
                 </ul>
-                <div class="settings" style="display:flex;flex-direction:column !important;"></div>
+                <div class="settings" style="display:flex;flex-direction:column !important; margin:10px !important"></div>
             </div>
         `);
         gooset = document.querySelector(".gooset");
